eb1d6014973a134c1fb73cee8932c099233efe21
Metellus <metellusjunior56@gmail.com>
2025-04-02 09:58:22 -0400

    Metellus complete lab

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+  }
+
+function makeInt(string){
+
+}
+
+function subtract(a, b) {
+    return a - b;
+  }
+
+function multiply(a, b) {
+    return a * b;
+  }
+
+function divide(a, b) {
+    return a / b;
+  }
+
+function increment(n){
+    n +=1;
+    return n;
+}
+
+function decrement(n){
+    n -=1;
+    return n--;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n='2.222'){
+    return parseFloat(n,10);
+}
